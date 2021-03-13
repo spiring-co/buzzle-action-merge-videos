5d3aa4b61cc5f462c9a340e65a4b75ca7bda7d1f
AbdulBsit <abasit325@gmail.com>
2021-03-13 12:47:00 +0530

    updated action

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
-const concat = require("ffmpeg-concat");
-const path = require('path');
+const ffmpeg = require("fluent-ffmpeg");
+const fetch = require("node-fetch");
+const nfp = require("node-fetch-progress");
+const fs = require("fs");
+const path = require("path");
+const ffprobe = require('ffprobe');
+const ffprobeStatic = require('ffprobe-static');
+
+const getBinary = (job, settings) => {
+  return new Promise((resolve, reject) => {
+    const version = "b4.2.2";
+    const filename = `ffmpeg-${version}${process.platform == "win32" ? ".exe" : ""
+      }`;
+    const fileurl = `https://github.com/eugeneware/ffmpeg-static/releases/download/${version}/${process.platform}-x64`;
+    const output = path.join(settings.workpath, filename);
+
+    if (fs.existsSync(output)) {
+      settings.logger.log(
+        `> using an existing ffmpeg binary ${version} at: ${output}`
+      );
+      return resolve(output);
+    }
+
+    settings.logger.log(`> ffmpeg binary ${version} is not found`);
+    settings.logger.log(
+      `> downloading a new ffmpeg binary ${version} to: ${output}`
+    );
+
+    const errorHandler = (error) =>
+      reject(
+        new Error({
+          reason: "Unable to download file",
+          meta: { fileurl, error },
+        })
+      );
+
+    fetch(fileurl)
+      .then((res) =>
+        res.ok
+          ? res
+          : Promise.reject({
+            reason: "Initial error downloading file",
+            meta: { fileurl, error: res.error },
+          })
+      )
+      .then((res) => {
+        const progress = new nfp(res);
+
+        progress.on("progress", (p) => {
+          process.stdout.write(
+            `${Math.floor(p.progress * 100)}% - ${p.doneh}/${p.totalh} - ${p.rateh
+            } - ${p.etah}                       \r`
+          );
+        });
+
+        const stream = fs.createWriteStream(output);
+
+        res.body.on("error", errorHandler).pipe(stream);
+
+        stream.on("error", errorHandler).on("finish", () => {
+          settings.logger.log(
+            `> ffmpeg binary ${version} was successfully downloaded`
+          );
+          fs.chmodSync(output, 0o755);
+          resolve(output);
+        });
+      });
+  });
+};
+
 
 module.exports = (job, settings, { input, input2, output, onStart, onComplete }) => {
   onStart()
+  let started = Date.now()
   return new Promise((resolve, reject) => {
     input = input || job.output;
     output = output || "mergedVideo.mp4";
@@ -16,29 +85,44 @@ module.exports = (job, settings, { input, input2, output, onStart, onComplete })
     if (output.indexOf("http") !== 0 && !path.isAbsolute(output)) {
       output = path.join(job.workpath, output);
     }
-
     settings.logger.log(
-      `[${job.uid}] starting action-mergeVideos on [${input}] `
+      `[${job.uid}] starting buzzle-action-merge-videos on [${input}] `
     );
-
-    concat({
-      output,
-      videos: [input2, input],
-      transition: {
-        name: "directionalWipe",
-        duration: 300,
-      },
-    })
-      .then((result) => {
-        console.log(result);
-        job.output = output;
+    getBinary(job, settings).then(async (p) => {
+      ffmpeg.setFfmpegPath(p);
+      const videoDetails = (await ffprobe(input, { path: ffprobeStatic.path })).streams.find(({ codec_type }) => codec_type === 'video')
+      const video1Details = (await ffprobe(input2, { path: ffprobeStatic.path })).streams.find((({ codec_type }) => codec_type === 'video'))
+      if (videoDetails.width === video1Details.width && videoDetails.height === video1Details.height) {
+        // run ffmpeg directly
+        ffmpeg()
+          .input(input)
+          .input(input2)
+          .outputOptions([`-r ${video1Details.r_frame_rate || 24}`])
+          .complexFilter('concat=n=2:v=1:a=1')
+          .on("error", function (err, stdout, stderr) {
+            settings.logger.log("join thumbnail video failed: " + err.message);
+            onComplete()
+            reject(err);
+          })
+          .on("progress", function (value) {
+            console.log("In Progress..");
+          })
+          .on("end", function () {
+            onComplete()
+            job.output = output;
+            settings.logger.log(
+              `[${job.uid}] Finished buzzle-action-merge-videos on [${input}] in ${(Date.now() - started) / 1000} secs `
+            );
+            resolve(job);
+          })
+          .save(output);
+      } else {
         onComplete()
-        resolve(output);
-      })
-      .catch((err) => {
-        console.log(err);
-        onComplete()
-        reject(err);
-      });
+        settings.logger.log(
+          `[${job.uid}] Exiting buzzle-action-merge-videos on [${input}] because of mismatch dimensions, took ${(Date.now() - started) / 1000} secs `
+        );
+        resolve(job);
+      }
+    }).catch(e => settings.logger.log(e.message))
   });
 };
--- sandbox.js
+++ sandbox.js
@@ -1,40 +1,17 @@
-const concat = require("ffmpeg-concat");
-
-module.exports = (job, settings, { input, video, output }) => {
-  return new Promise((resolve, reject) => {
-    input = input || job.output;
-    output = output || "mergedVideo.mp4";
-
-    if (input.indexOf("http") !== 0 && !path.isAbsolute(input)) {
-      input = path.join(job.workpath, input);
-    }
-    if (video.indexOf("http") !== 0 && !path.isAbsolute(video)) {
-      video = path.join(job.workpath, video);
-    }
-    if (output.indexOf("http") !== 0 && !path.isAbsolute(output)) {
-      output = path.join(job.workpath, output);
-    }
-
-    settings.logger.log(
-      `[${job.uid}] starting action-mergeVideos on [${input}] `
-    );
-
-    concat({
-      output,
-      videos: [video, input],
-      transition: {
-        name: "directionalWipe",
-        duration: 300,
-      },
-    })
-      .then((result) => {
-        console.log(result);
-        job.output = output;
-        resolve(output);
-      })
-      .catch((err) => {
-        console.log(err);
-        reject(err);
-      });
-  });
-};
+const mergedVideo = require("./index")
+const output = "mergedVideo.mp4";
+let input = 'ganesh_opening_new(960x540).mp4'
+const input2 = 'rendered.mp4'
+let started = Date.now()
+mergedVideo(
+  { output: "C:\\Users\\Utkarsh\\Desktop", workpath: "C:\\Users\\Utkarsh\\Desktop" }, {
+  logger: { log: console.log },
+  workpath: "C:\\Users\\Utkarsh\\Desktop"
+}, {
+  input, input2, output,
+  onStart: () => {
+    console.log("Started")
+    started = Date.now()
+  },
+  onComplete: () => console.log("completed in", (Date.now() - started) / 1000, " secs")
+}).catch((e) => console.log("Something went wrong! ", e.message))
